ad3eef8fd67bc82f64d01fa41f40fc091b184d8f
tiwariParth <243mjparth@gmail.com>
2024-08-13 20:08:56 +0530

    Refactor code to handle user input in guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,11 @@
-use std::io;
+use std::io::{self, Read};
 
 fn main(){
     println!("guess the number!");
     println!("Please input your guess");
 
     let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read Line");
+
 }
