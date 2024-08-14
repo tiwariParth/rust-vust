dc09df1ab438095540d7500c4e4aeacd5e586953
tiwariParth <243mjparth@gmail.com>
2024-08-14 07:41:03 +0530

    Refactor guessing game to use rand crate for generating secret number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,34 @@
-use std::io::{self};
+use core::num;
+use std::{cmp::Ordering, io::{self}};
+
+use rand::Rng;
 
 fn main(){
     println!("guess the number!");
-    println!("Please input your guess");
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    println!("The secret number is: {secret_number}");
+    loop{
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read Line");
+        io::stdin().read_line(&mut guess).expect("Failed to read Line");
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_) => continue,
+        }; 
 
-    println!("You guessed: {}",guess);
+        println!("You guessed: {}",guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+            Ordering::Greater => println!("Too Big")
+        }
+    }
 
 
 }
