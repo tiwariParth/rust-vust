32e1461a42a0fb09dc209647a7e4665658fb8f93
tiwariParth <243mjparth@gmail.com>
2024-08-14 07:41:13 +0530

    Refactor guessing game to remove unnecessary print statement

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,7 +6,6 @@ use rand::Rng;
 fn main(){
     println!("guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    println!("The secret number is: {secret_number}");
     loop{
         println!("Please input your guess");
 
